9445f71beaab1a511aa44fb04b0804e2b41896f1
imhotep <r.sipchenko@gmail.com>
2024-03-10 14:26:54 -0600

    update site name

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Web Hero";
+export const SITE_TITLE = "Frontend Stack Lab";
 export const SITE_DESCRIPTION =
-  "Explore cutting-edge web development insights at Web Hero Blog! Our site offers comprehensive tutorials, expert advice on interview questions, and the latest trends in modern web development.";
+  "Explore cutting-edge web development insights at Frontend Stack Lab! Our site offers comprehensive tutorials, expert advice on interview questions, and the latest trends in modern web development.";
 export const TWITTER_HANDLE = "@RSypchenko";
-export const MY_NAME = "Web Hero";
+export const MY_NAME = "FrontendStackLab";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
